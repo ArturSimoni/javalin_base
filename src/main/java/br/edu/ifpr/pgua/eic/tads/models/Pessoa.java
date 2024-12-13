1e7432c4bda3c67c0d243a991ace5867adce8ff0
Artur Simoni <artursimonijesus@gmail.com>
2024-12-13 12:40:58 -0300

    Atividade POO SITE CADASTRO

--- src/main/java/br/edu/ifpr/pgua/eic/tads/models/Pessoa.java
+++ src/main/java/br/edu/ifpr/pgua/eic/tads/models/Pessoa.java
@@ -2,18 +2,36 @@ package br.edu.ifpr.pgua.eic.tads.models;
 
 public class Pessoa {
     private String nome;
-    private String cpf;
+    private String telefone;
+    private String email;
 
-    public Pessoa(String nome, String cpf){
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+    public Pessoa(String nome, String telefone, String email){
         this.nome = nome;
-        this.cpf = cpf;
+        this.telefone=telefone;
+        this.email=email;
     }
 
     public String getNome(){
         return this.nome;
     }
 
-    public String getCpf(){
-        return this.cpf;
-    }
+   
 }
